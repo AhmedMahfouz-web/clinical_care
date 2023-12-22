62c8a27af809d9c2494653853351cd7f0449ff31
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2023-12-22 07:38:51 +0300

    report

--- database/migrations/2023_12_22_033039_create_reports_table.php
+++ database/migrations/2023_12_22_033039_create_reports_table.php
@@ -13,10 +13,6 @@ return new class extends Migration
     {
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
-            $table->id();
-            $table->id();
-            $table->id();
-            $table->id();
             $table->timestamps();
         });
     }
