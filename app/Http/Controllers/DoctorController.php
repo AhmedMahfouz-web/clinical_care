52a3db6583fc2638a6e06e44b555dc1eb210e81a
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2023-12-28 00:00:28 +0300

    doctors

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -39,7 +39,7 @@ class DoctorController extends Controller
 
     public function profile()
     {
-        $doctor = auth()->guard('doctor')->user;
+        $doctor = auth('doctor')->user();
         return response()->json([
             'status' => 'success',
             'doctor' => $doctor
