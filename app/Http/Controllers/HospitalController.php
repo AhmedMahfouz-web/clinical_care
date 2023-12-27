264c85db93ed40eec1e348e3ad5c0128f3a380d7
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2023-12-27 09:06:30 +0300

    reservation

--- app/Http/Controllers/HospitalController.php
+++ app/Http/Controllers/HospitalController.php
@@ -26,7 +26,7 @@ class HospitalController extends Controller
             'type' => 'required|string|max:255',
             'address' => 'required|string|max:255',
             'city' => 'required|string|max:255',
-            'phone' => 'required|numric|max:255',
+            'phone' => 'required|numeric|max:255',
             'bio' => 'required|string|max:255',
         ]);
 
