258ffa11f99222b7c99a2610e8d278fb17cee042
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2023-12-28 17:33:29 +0300

    token

--- routes/api.php
+++ routes/api.php
@@ -86,3 +86,5 @@ Route::middleware(['auth:doctor'])->group(function () {
 
 Route::get('get_professions', [ProfessionController::class, 'index_api']);
 Route::get('get_partners', [PartnerController::class, 'index_api']);
+
+Route::get('/token', [MeetingController::class, 'start_meeting']);
