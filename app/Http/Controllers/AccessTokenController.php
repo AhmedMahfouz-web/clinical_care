8b5bfb8e5c9122167b9014c4a16d1ab52fd50f85
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2024-01-05 23:32:09 +0300

    twillio;

--- app/Http/Controllers/AccessTokenController.php
+++ app/Http/Controllers/AccessTokenController.php
@@ -8,14 +8,14 @@ use Twilio\Jwt\Grants\VideoGrant;
 
 class AccessTokenController extends Controller
 {
-    public function generate_token()
+    public function generate_token(Request $request)
     {
         // Substitute your Twilio Account SID and API Key details
-        $accountSid = env('TWILIO_ACCOUNT_SID');
-        $apiKeySid = env('TWILIO_API_KEY_SID');
-        $apiKeySecret = env('TWILIO_API_KEY_SECRET');
-
-        $identity = uniqid();
+        $accountSid = ('TWILIO_ACCOUNT_SID');
+        $apiKeySid = ('TWILIO_API_KEY');
+        $apiKeySecret = ('TWILIO_API_SECRET');
+        $identity = $request->identity;
+        $room_name = $request->room_name;
 
         // Create an Access Token
         $token = new AccessToken(
@@ -23,15 +23,21 @@ class AccessTokenController extends Controller
             $apiKeySid,
             $apiKeySecret,
             3600,
-            $identity
+            $identity,
+            $room_name
         );
 
         // Grant access to Video
         $grant = new VideoGrant();
-        $grant->setRoom('cool room');
+        // $grant->setRoom('');
         $token->addGrant($grant);
 
         // Serialize the token as a JWT
-        echo $token->toJWT();
+        $result = [
+            "identity" => $identity,
+            "token" => $token->toJWT()
+        ];
+
+        return response()->json($result);
     }
 }
