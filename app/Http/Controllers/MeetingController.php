7752ad25459f359d10d7a45cb2512c53deb144db
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2024-01-05 05:00:31 +0300

    zoom;

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -73,11 +73,9 @@ class MeetingController extends Controller
             "type" => 2, // 1 => instant, 2 => scheduled, 3 => recurring with no fixed time, 8 => recurring with fixed time
             "duration" => 60, // in minutes
             "timezone" => 'Asia/Dhaka', // set your timezone
-            "password" => 'set your password',
-            "start_time" => 'set your start time', // set your start time
+            "password" => '',
             "template_id" => 'set your template id', // set your template id  Ex: "Dv4YdINdTk+Z5RToadh5ug==" from https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetingtemplates
             "pre_schedule" => false,  // set true if you want to create a pre-scheduled meeting
-            "schedule_for" => 'set your schedule for profile email ', // set your schedule for
             "settings" => [
                 'join_before_host' => false, // if you want to join before host set true otherwise set false
                 'host_video' => false, // if you want to start video when host join set true otherwise set false
