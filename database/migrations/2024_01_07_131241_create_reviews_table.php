4b8b637f15311e063a1e80384b731a11ca271202
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2024-01-08 05:10:21 +0300

    update

--- database/migrations/2024_01_07_131241_create_reviews_table.php
+++ database/migrations/2024_01_07_131241_create_reviews_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
             $table->foreignUuid('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->numeric();
-            $table->id();
             $table->timestamps();
         });
     }
