52438b357f2d1a45bff35debc4ae95848dfef571
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2024-01-14 01:38:23 +0300

    meeting

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -64,7 +64,7 @@ class MeetingController extends Controller
     {
         $meeting->load(['files', 'user']);
         $doctors = Doctor::where('profession', $meeting->profession)->get();
-        return view('pages.meeting.show_answered', compact(['meeting', 'doctors']));
+        return view('pages.meetings.show_answered', compact(['meeting', 'doctors']));
     }
 
     public function assign_doctor(Request $request, Meeting $meeting)
